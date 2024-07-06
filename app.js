72f030d664aec218e649f517c53ee69ff2a01795
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-07-06 15:41:49 +0300

    install fingerprintjs2 and get the device ID

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ const PORT = 3000;
 const app = express()
 const path = require('path')
 // app.use(express.static(path.json(__dirname, 'public')))
+app.use((req,res,next) => {
+    const userAgent = req.get('user-agent')
+    const Fingerprint2 = require('fingerprintjs2')
+    Fingerprint2.getPromise().then(components => {
+        const fingerprint = Fingerprint2.x64hash128(components.map(pair => pair.value).join(''), 31)
+        req.deviceId = fingerprint;
+        next();
+    })
+})
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname, 'public', 'index.html')
+    res.send('DeviceId:' + req.deviceId)
 })
 app.listen(PORT, (req,res)=>{
     console.log(`Server works on PORT: ${PORT}`)
